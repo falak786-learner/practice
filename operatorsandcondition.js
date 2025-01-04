a4eefc0271334e09867fa6c068cde0c75658b72d
falak786-learner <aksa786ansari@gmail.com>
2025-01-04 22:16:51 +0530

    javascrit beginner

---
+++ operatorsandcondition.js
@@ -0,0 +1,60 @@
+//Arithmetic operators
+console.log ("welcome to console");
+
+//operators
+// let a = 5 ;
+// let b = 2;
+
+// console.log ("a + b =", a + b); // addition
+// console.log ("a + b =", a - b); //subtraction
+// console.log ("a + b =", a / b); //division
+// console.log ("a + b =", a % b); //modulas
+// console.log ("a + b =", a ** b); //Exponantional
+
+//unary operators i.e increment and decrement
+// let a = 5 ;
+// let b = 2;
+// console.log ("a++ = " , a++); //post increment a++ = 5
+// console.log ("a =" , a); //6
+// console.log ("a++ = " , ++a); //pre increment ++a = 7
+// console.log ("a =" , a); //7
+// console.log ("a++ = " , a--); //post decrement a-- = 7
+// console.log ("a =" , a); //6
+// console.log ("a++ = " , --a); //pre decrement --a = 5
+// console.log ("a =" , a); //5
+
+//Assignment operators
+/* let a = 5 ;
+let b = 2;*/
+
+// a += 4; //a = a + 4
+// console.log ("a = " , a) ; //9
+
+// a -= 4; //a = a - 4
+// console.log ("a = " , a) ; //5
+
+// // a /= 4; //a = a / 4
+// // console.log ("a = " , a) ; //1.25
+
+// // a %= 4; //a = a % 4
+// // console.log ("a = " , a) ; //1
+
+// a **= 4; //a = a ** 4
+// console.log ("a = " , a) ; //625
+
+//Comparison operators
+// Equal to ==      ,    Equal to & type ===
+// Not equal to !=   ,    Not equal to & type !==
+// > , < , <= , >= , =
+let a = 5 ;
+let b = "2";
+
+
+// LOGICAL OPERATORS
+//Comparing two or multiple conditions
+// AND && = both should be true
+// OR || = dono ma sa ak true hona chaiya
+// NOT ! = negation
+
+
+
